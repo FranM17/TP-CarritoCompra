4768a2bec8c1f7c17c68b5b7ca4049500e8fe54e
Franco Mondino <franmondi17@gmail.com>
2025-06-16 16:41:31 -0300

    Agrego Categoría e ItemCarrito

--- CarritoCompras/Categoria.cs
+++ CarritoCompras/Categoria.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using CarritoCompras;
+
+Categoria.cs:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarritoCompras
+{
+    public class Categoria
+    {
+        public string Nombre { get; }
+        public string Descripcion { get; }
+
+        public Categoria(string nombre, string descripcion)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+        }
+    }
+}
--- CarritoCompras/ItemCarrito.cs
+++ CarritoCompras/ItemCarrito.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using CarritoCompras;
+
+ItemCarrito.cs:
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarritoCompras
+{
+    public class ItemCarrito
+    {
+        public Producto Producto { get; }
+        public int Cantidad { get; private set; }
+
+        public ItemCarrito(Producto producto, int cantidad)
+        {
+            Producto = producto;
+            Cantidad = cantidad;
+        }
+
+        public void AumentarCantidad(int cantidad)
+        {
+            Cantidad += cantidad;
+        }
+
+        public decimal Subtotal()
+        {
+            var total = Producto.Precio * Cantidad;
+            if (Cantidad >= 5)
+                total *= 0.85m;
+            return total;
+        }
+    }
+}
